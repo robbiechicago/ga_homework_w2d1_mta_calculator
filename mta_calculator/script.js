e9104db2dec97a793334ed6e89ef7b3f0e4b0065
Rob Holloway <robbiechicago@gmail.com>
2015-08-18 21:56:37 +0100

    moved from one line to all lines, created dynamic selectors for start and end stations.  The end station selector does not include the start station.

--- mta_calculator/script.js
+++ mta_calculator/script.js
@@ -3,28 +3,37 @@ console.log("js linked: MTA stops calc");
 
 //subway line arrays
 var line_l = ['8th', '6th', 'Union Square', '3rd', '1st'];
-var line_n = ['Times Square', '34th', 'N-28th', 'N-23rd', 'Union Square', '8th'];
-var line_6 = ['Grand Central', '33rd', '6-28th', '6-23rd', 'Union Square', 'Astor Place'];
+var line_n = ['Times Square', '34th', '28th (N-Line)', '23rd (N-Line)', 'Union Square', '8th'];
+var line_6 = ['Grand Central', '33rd', '28th (6-Line)', '23rd (6-Line)', 'Union Square', 'Astor Place'];
+var all_lines_dup = line_l.concat(line_n, line_6);
+var all_lines = all_lines_dup.filter(function(elem, pos){
+  return all_lines_dup.indexOf(elem) === pos;
+}).sort();
 
 //Dynamically fill selects
-var lls = document.getElementById('line-l-start');
-for(var i = 0, len = line_l.length; i < len; i++) {
+var start_selector = document.getElementById('start-station');
+for(var i = 0, len = all_lines.length; i < len; i++) {
   var opt = document.createElement('option');
-  opt.innerHTML = line_l[i];
-  opt.value = line_l[i];
-  lls.appendChild(opt);
+  opt.innerHTML = all_lines[i];
+  opt.value = all_lines[i];
+  start_selector.appendChild(opt);
 }
-var lls_change = lls.addEventListener('change', function(){
-  console.log(lls.options[lls.selectedIndex].value);
 
+var start_change = start_selector.addEventListener('change', function(){
+  console.log(start_selector.options[start_selector.selectedIndex].value);
+  end_stations();
 })
 
-var lle = document.getElementById('line-l-end');
-for(var i = 0, len = line_l.length; i < len; i++) {
-  var opt = document.createElement('option');
-  opt.innerHTML = line_l[i];
-  opt.value = line_l[i];
-  lle.appendChild(opt);
+function end_stations() {
+  var end_selector = document.getElementById('end-station');
+  var startIndex = all_lines.indexOf(start_selector.options[start_selector.selectedIndex].value);
+  var removeStart = all_lines.splice(startIndex,1);
+  for(var i = 0, len = all_lines.length; i < len; i++) {
+    var opt = document.createElement('option');
+    opt.innerHTML = all_lines[i];
+    opt.value = all_lines[i];
+    end_selector.appendChild(opt);
+  }
 }
 
 
@@ -35,15 +44,14 @@ var l_boom = document.getElementById('l-boom').addEventListener('click', stopCal
 
 function stopCalc(start, end) {
   console.log("function stopCalc successfully called");
-  console.log("start station: " + lls.options[lls.selectedIndex].value);
-  console.log("end station: " + lle.options[lle.selectedIndex].value);
+  var start_location = start_selector.options[start_selector.selectedIndex].value;
+  var end_location = end_selector.options[end_selector.selectedIndex].value;
+  console.log("start station: " + start_location);
+  console.log("end station: " + end_location);
+  result = Math.abs(line_l.indexOf(start_location) - line_l.indexOf(end_location));
+  console.log(result);
+  document.getElementById('result').innerHTML = result;
 }
-
-
-
-
-
-
 
 
 
