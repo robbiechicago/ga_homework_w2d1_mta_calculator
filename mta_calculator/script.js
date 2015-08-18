909e2d81d8b6fdc4323edfac0fdba7699315366a
Rob Holloway <robbiechicago@gmail.com>
2015-08-18 18:04:09 +0100

    Initial commit.  created html, css and js pages, linked them all, and created line arrays

---
+++ mta_calculator/script.js
@@ -0,0 +1,37 @@
+ window.onload = function() {
+
+var line_l = ['8th', '6th', 'Union Square', '3rd', '1st'];
+var line_n = ['Times Square', '34th', 'N-28th', 'N-23rd', 'Union Square', '8th'];
+var line_6 = ['Grand Central', '33rd', '6-28th', '6-23rd', 'Union Square', 'Astor Place'];
+
+
+console.log("Arse");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} //END OF WINDOW.ONLOAD
